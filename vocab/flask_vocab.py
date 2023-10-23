32557e510900b66184dfc12e749be78f1f75b404
Elijah <ethomas1541@gmail.com>
2023-10-22 18:20:50 -0700

    Project 3

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -12,6 +12,7 @@ from src.letterbag import LetterBag
 from src.vocab import Vocab
 from src.jumble import jumbled
 import src.config as config
+from time import sleep
 
 
 ###
@@ -68,10 +69,10 @@ def keep_going():
     return flask.render_template('vocab.html')
 
 
-@app.route("/success")
+@app.route("/success", methods = ["GET"])
 def success():
+    print("\nYEAH\n\n")
     return flask.render_template('success.html')
-
 
 #######################
 # Form handler.
@@ -79,7 +80,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check", methods = ["GET"])
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -92,51 +93,36 @@ def check():
     app.logger.debug("Entering check")
 
     # The data we need, from form and from cookie
-    text = flask.request.form["attempt"]
+    text = flask.request.args.get("text", type=str)
+
     jumble = flask.session["jumble"]
     matches = flask.session.get("matches", [])  # Default to empty list
+
+    print(jumble)
 
     # Is it good?
     in_jumble = LetterBag(jumble).contains(text)
     matched = WORDS.has(text)
+    already_matched = False
+    complete = False
 
     # Respond appropriately
     if matched and in_jumble and not (text in matches):
         # Cool, they found a new word
+        matches = flask.session.get("matches", [])
         matches.append(text)
         flask.session["matches"] = matches
     elif text in matches:
-        flask.flash("You already found {}".format(text))
-    elif not matched:
-        flask.flash("{} isn't in the list of words".format(text))
-    elif not in_jumble:
-        flask.flash(
-            '"{}" can\'t be made from the letters {}'.format(text, jumble))
-    else:
-        app.logger.debug("This case shouldn't happen!")
-        assert False  # Raises AssertionError
+        already_matched = True
 
     # Choose page:  Solved enough, or keep going?
-    if len(matches) >= flask.session["target_count"]:
-       return flask.redirect(flask.url_for("success"))
-    else:
-       return flask.redirect(flask.url_for("keep_going"))
 
-
-###############
-# AJAX request handlers
-#   These return JSON, rather than rendering pages.
-###############
-
-@app.route("/_example")
-def example():
-    """
-    Example ajax request handler
-    """
-    app.logger.debug("Got a JSON request")
-    rslt = {"key": "value"}
+    if len(matches) >= CONFIG.SUCCESS_AT_COUNT:
+        complete = True
+    
+    rslt = {"has": matched, "inJumble": in_jumble, "alreadyHas": already_matched, "complete": complete}
+    app.logger.debug(text + " " + str(rslt) + str(matches))
     return flask.jsonify(result=rslt)
-
 
 #################
 # Functions used within the templates
